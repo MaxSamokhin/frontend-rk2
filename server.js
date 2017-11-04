e971f07359eb84fbced3e7eac993582c6ef92536
Max Samokhin <samokhinmax@gmail.com>
2017-11-04 21:37:08 +0300

    fix port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 app.use('/', express.static('./src'));
 
-const port = 8001;
+const port = process.env.PORT || 8001;
 
 app.listen(port, () => {
     console.log(`App start on port ${port}`);
